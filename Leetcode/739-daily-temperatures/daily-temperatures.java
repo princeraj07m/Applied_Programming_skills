a02f927276f4bab6f01887074e118135101c067c
Prince kumar <princerajm31@gmail.com>
2026-02-16 10:14:40 +0530

    Time: 79 ms (12.47%) | Memory: 106.8 MB (66.97%) - LeetSync

---
+++ Leetcode/739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temps) {
+        int n = temps.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temps[stack.peek()] <= temps[i]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                result[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
