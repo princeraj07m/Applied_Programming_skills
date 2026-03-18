14f6d54c6738028f64c1e3182d616c70704aa0fa
Prince kumar <princerajm31@gmail.com>
2026-03-19 02:41:42 +0530

    Time: 2 ms (99.46%) | Memory: 72.5 MB (44.34%) - LeetSync

---
+++ Leetcode/890-lemonade-change/lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+     
+        int five = 0,ten = 0,fifteen=0,twenty =0;
+        for(var num : bills){
+            if(num == 5){
+                five++;
+            }else if(num == 10){
+                if(five<1){
+                    return false;
+                }
+                five--;
+                ten++;
+            }else if(num == 20){
+                if(ten > 0 && five >0){
+                    ten--;
+                    five--;
+                }else if(five>2){
+                    five = five - 3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
