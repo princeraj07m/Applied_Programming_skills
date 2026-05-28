ae3caef7f35075badfc60808bf2a89881c86939a
Prince kumar <princerajm31@gmail.com>
2026-05-28 23:18:47 +0530

    Time: 1 ms (99.61%) | Memory: 47.4 MB (20.49%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,26 +1,17 @@
 class Solution {
-
-    static int f(int idx, int[] nums, int[] dp) {
-
-        if (idx >= nums.length - 1) return 0;
-
-        if (dp[idx] != -1) return dp[idx];
-
-        int mini = (int)1e9;
-
-        for (int i = 1; i <= nums[idx]; i++) {
-            mini = Math.min(mini, 1 + f(idx + i, nums, dp));
-        }
-
-        return dp[idx] = mini;
-    }
-
     public int jump(int[] nums) {
-
-        int[] dp = new int[nums.length];
-
-        Arrays.fill(dp, -1);
-
-        return f(0, nums, dp);
+        int ans = 0;
+        int l=0;
+        int r = 0;
+        while(r<nums.length-1){
+            int farthest = 0;
+            for(int i =l;i<=r;i++){
+                farthest = Math.max(farthest,i+nums[i]);
+            }
+            l=r+1;
+            r=farthest;
+            ans++;
+        }
+        return ans;
     }
 }
