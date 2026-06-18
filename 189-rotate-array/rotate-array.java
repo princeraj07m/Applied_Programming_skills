09b9ac67aec7bc43eec657aa1d3e93cadb81ed62
Prince kumar <princerajm31@gmail.com>
2026-06-19 02:06:46 +0530

    Time: 0 ms (100.00%) | Memory: 61.2 MB (54.26%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -10,10 +10,14 @@ class Solution {
     }
     public void rotate(int[] nums, int k) {
         k = k%nums.length;
+        // array ko plt denge
         reverse(nums,0,nums.length-1);
+
+        //phle k element plt denge
         int l = 0;
         int r = k-1;
         reverse(nums,l,r);
+        //bache hue element plt denge
         l = k;
         r = nums.length-1;
         reverse(nums,l,r);
