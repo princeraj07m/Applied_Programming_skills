e8ca8a3d1220662c720e6d2d895259c4bdba4998
Prince kumar <princerajm31@gmail.com>
2026-06-19 15:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (87.28%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max =0;
+        for(int i =0;i<accounts.length;i++){
+            int curr =0;
+            for(int j = 0;j<accounts[0].length;j++){
+                curr=curr+accounts[i][j];
+            }
+            max = Math.max(max,curr);
+        }
+        return max;
+    }
+}
