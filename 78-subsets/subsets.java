63c15c2174234c9eb9ba5160337a68e36138a116
Prince kumar <princerajm31@gmail.com>
2026-06-25 01:38:16 +0530

    Time: 2 ms (5.39%) | Memory: 44.5 MB (7.39%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    void f(int[] nums, int i, int n, List<Integer> ds, HashSet<List<Integer>> ans) {
+        if (i >= n) {
+            List<Integer> temp = new ArrayList<>(ds);
+            Collections.sort(temp);
+            ans.add(temp);
+            return;
+        }
+
+        ds.add(nums[i]);
+        f(nums, i + 1, n, ds, ans);
+
+        ds.remove(ds.size() - 1);
+        f(nums, i + 1, n, ds, ans);
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        HashSet<List<Integer>> ans = new HashSet<>();
+        List<Integer> ds = new ArrayList<>();
+        f(nums, 0, n, ds, ans);
+
+        List<List<Integer>> fans = new ArrayList<>(ans);
+        return fans;
+    }
+}
