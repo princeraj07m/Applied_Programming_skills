70c0d243f9bd61958d5cbe21f0ea73c9af2f3813
Prince kumar <princerajm31@gmail.com>
2026-06-26 10:53:02 +0530

    Time: 2 ms (83.77%) | Memory: 46.8 MB (27.87%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,19 +1,13 @@
 class Solution {
     boolean isSafe(int row, int col, char[][]board,int n){
         for(int i = row-1;i>=0;i--){
-            if(board[i][col] == 'Q'){
-                return false;
-            }
+            if(board[i][col] == 'Q')return false;
         }
-        for(int i = row-1,j = col-1;i>=0&&j>=0;i--,j--){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
+        for(int i = row-1,j=col-1;i>=0&&j>=0;i--,j--){
+            if(board[i][j] == 'Q')return false;
         }
-        for(int i = row-1,j = col+1;i>=0&&j<n;i--,j++){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
+        for(int i = row-1,j=col+1;i>=0&&j<n;i--,j++){
+            if(board[i][j] == 'Q')return false;
         }
         return true;
     }
@@ -31,11 +25,12 @@ class Solution {
         int i = row;
         for(int j =0;j<n;j++){
             if(isSafe(i,j,board,n)){
-                board[i][j]='Q';
-                f(row+1,n,board,ans);
-                board[i][j]= '.';
+                board[i][j] = 'Q';
+                f(i+1,n,board,ans);
+                board[i][j] = '.';
             }
         }
+        
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
