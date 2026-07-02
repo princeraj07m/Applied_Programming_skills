ea3cb675bc20559cf3e9e35bfc11d396a9114d1b
Prince kumar <princerajm31@gmail.com>
2026-07-02 13:50:03 +0530

    Time: 1 ms (96.26%) | Memory: 46.5 MB (91.30%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,29 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) return ans;
+        List<List<Integer>> ans  = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        if(root==null)return ans;
 
-        Deque<TreeNode> dq = new ArrayDeque<>();
-        dq.offer(root);
-
-        while (!dq.isEmpty()) {
-            int size = dq.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = dq.poll();
-                level.add(node.val);
-
-                if (node.left != null) {
-                    dq.offer(node.left);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> curr = new ArrayList<>();
+            for(int i =0;i<levelNum;i++){
+                TreeNode temp = queue.peek();
+                if(temp.left!=null){
+                    queue.offer(temp.left);
                 }
-                if (node.right != null) {
-                    dq.offer(node.right);
+                if(temp.right!=null){
+                    queue.offer(temp.right);
                 }
+                curr.add(queue.poll().val);
             }
-            ans.add(level);
+            ans.add(curr);
         }
         return ans;
     }
 }
+
+
+
+
+
+
+
