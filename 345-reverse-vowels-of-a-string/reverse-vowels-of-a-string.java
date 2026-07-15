1b6eaf968371fb1f6e29bf08edf427c7e0f022cd
Prince kumar <princerajm31@gmail.com>
2026-07-15 22:41:32 +0530

    Time: 4 ms (33.03%) | Memory: 46.8 MB (34.28%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -6,7 +6,7 @@ class Solution {
         mp.add('a');mp.add('e');mp.add('i');mp.add('o');mp.add('u');
         mp.add('A');mp.add('E');mp.add('I');mp.add('O');mp.add('U');
         for(int i =0;i<sb.length() && j >=0;){
-            if(i>=j)break;
+            // if(i>=j)break;
             while(i<sb.length() && !mp.contains(sb.charAt(i)))i++;
             while(j>=0 && !mp.contains(sb.charAt(j)))j--;
             if(i>=j)break;
